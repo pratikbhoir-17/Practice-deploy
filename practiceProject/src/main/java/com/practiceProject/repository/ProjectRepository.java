1760d28e2a171f65e23056bde1fe96e386156e1c
test <test@DESKTOP-DLISGLN>
2023-12-18 11:11:41 +0530

    add project repository

--- practiceProject/src/main/java/com/practiceProject/repository/ProjectRepository.java
+++ practiceProject/src/main/java/com/practiceProject/repository/ProjectRepository.java
@@ -1,11 +1,16 @@
 package com.practiceProject.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.practiceProject.model.Projects;
 
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Projects, Integer> {
-
+	public List<Projects> findByprojectId(String projectId); 
+	public List<Projects> findBystatus(String status);
+	//public Employee findBymailId(String mailId);
 }
