44dd5bea987c77b0d8a1227b3b0801bbfed4aaa6
test <test@DESKTOP-DLISGLN>
2023-12-18 11:04:11 +0530

    commit with some changes

--- practiceProject/src/main/java/com/practiceProject/controller/EmployeeController.java
+++ practiceProject/src/main/java/com/practiceProject/controller/EmployeeController.java
@@ -3,7 +3,6 @@ package com.practiceProject.controller;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,20 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.practiceProject.Dto.EmployeeDto;
 import com.practiceProject.Dto.LoginDto;
 import com.practiceProject.Dto.StatusDto;
 import com.practiceProject.model.Employee;
 import com.practiceProject.service.EmployeeService;
 import com.practiceProject.util.Constants;
-
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ValidatorFactory;
 
 
 @RestController
-
 public class EmployeeController {
 	@Autowired
 	public EmployeeService employeeService;
@@ -35,36 +31,32 @@ public class EmployeeController {
 		return "Welcome";
 
 	}
-	
+
 	@PostMapping("/login")
-	public ResponseEntity<?>login(@RequestBody LoginDto dto) {
+	public ResponseEntity<?> login(@RequestBody LoginDto dto) {
 		Employee emp = new Employee();
 		Logger logger = Logger.getLogger(EmployeeController.class);
-		 logger.info("Inside EmployeeController ==> login Api" + dto);
+		logger.info("Inside EmployeeController ==> login Api" + dto);
 		StatusDto response = new StatusDto();
-			try {
-				response = employeeService.login(dto);
-			} catch (Exception e1) {
-				//response.setJwttoken("");
-				//response.setStatus(Constants.FAIL);
-				response.setCode(Constants.ADSS101);
-				response.setMessage(Constants.INVALID_EMAIL);
-				//log.error(response);
-				// }
-			}
-		
-		
+		try {
+			response = employeeService.login(dto);
+		} catch (Exception e1) {
+			// response.setJwttoken("");
+			// response.setStatus(Constants.FAIL);
+			response.setCode(Constants.ADSS101);
+			response.setMessage(Constants.INVALID_EMAIL);
+			// log.error(response);
+			// }
+		}
 		return ResponseEntity.ok(response);
-		
-		
 	}
-	
+
 	@PostMapping("/addEmployee")
 	public ResponseEntity<?> addEmployee(@RequestBody EmployeeDto emp) {
-		 Logger logger = Logger.getLogger(EmployeeController.class);
-		 logger.info("Inside EmployeeController ==> addEmployee() " + emp);
-		 List<StatusDto> reslist = new ArrayList<StatusDto>();
-		 StatusDto response = new StatusDto();
+		Logger logger = Logger.getLogger(EmployeeController.class);
+		logger.info("Inside EmployeeController ==> addEmployee() " + emp);
+		List<StatusDto> reslist = new ArrayList<StatusDto>();
+		StatusDto response = new StatusDto();
 		ValidatorFactory factory = jakarta.validation.Validation.buildDefaultValidatorFactory();
 		jakarta.validation.Validator validator = factory.getValidator();
 		Set<ConstraintViolation<EmployeeDto>> violations = validator.validate(emp);
@@ -76,32 +68,31 @@ public class EmployeeController {
 			return ResponseEntity.ok(reslist);
 		}
 		if (reslist.isEmpty()) {
-		response=employeeService.addEmp(emp);
+			response = employeeService.addEmp(emp);
 		}
 
-		return ResponseEntity.ok (response);
-
+		return ResponseEntity.ok(response);
 	}
+
 	@GetMapping("findAllemployee")
 	public ResponseEntity<Object> getAllEmployee() {
-		//StatusDto responseDto = new StatusDto();
-		List<Employee>list=new ArrayList<Employee>();
+		Logger logger = Logger.getLogger(EmployeeController.class);
+		// StatusDto responseDto = new StatusDto();
+		List<Employee> list = new ArrayList<Employee>();
 
-//		LOGGER.info("Received getAllProject request");
+		logger.info("Received getAllProject request");
 		try {
 			list = employeeService.findAllEmployee();
 			return ResponseEntity.ok(list);
 		} catch (Exception e) {
-//			LOGGER.error("Error occurred during getAllProject: {}", e.getMessage(), e);
+			logger.error("Error occurred during getAllProject: {}", e);
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
 					.body("An error occurred during getAllProject");
 		}
 	}
-	
-	
+
 	@GetMapping("deleteemployee/{empId}")
-	public ResponseEntity<Object> deactivateProject(
-			@RequestBody EmployeeDto dto) {
+	public ResponseEntity<Object> deactivateProject(@RequestBody EmployeeDto dto) {
 		StatusDto responseDto = new StatusDto();
 
 		try {
@@ -113,6 +104,5 @@ public class EmployeeController {
 		}
 		return ResponseEntity.ok(responseDto);
 	}
-
 
 }
